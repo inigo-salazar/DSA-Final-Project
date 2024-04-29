4b5c7f4d5a5eb511ea2503cae1afb6e8e3015ea5
Jules Thaddeus Cruz <114631051+cruzj0@users.noreply.github.com>
2024-04-29 14:16:55 +0800

    First Commit
    chessboard skeleton code

---
+++ StartScreen.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*
+	Start screen
+		- contains title, "load game" button
+			- Title: borderlayout NORTH
+			- Button: borderlayout CENTER
+		- "load game" button puts a popup requesting username; contains input text field, submit button
+			- Text field: borderlayout CENTER
+			- Submit button: borderlayout CENTER
+*/
+
+public class StartScreen{
+	
+}
